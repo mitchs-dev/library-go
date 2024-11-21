681e27eba9ea960b5a3406f6603183842ff034f1
Mitchell Stanton <mitchell.stanton@corespark.io>
2024-11-21 12:49:18 -0500

    Update request schema configuration to include optional JSON fields as well as response/request headers

--- requestSchemas/requestSchemas.go
+++ requestSchemas/requestSchemas.go
@@ -11,21 +11,31 @@ import (
 // Struct to which the request schema configuration files should follow
 type Schema struct {
 	RequestSchema struct {
-		SchemaVersion string `yaml:"schemaVersion"`
+		SchemaVersion string `yaml:"schemaVersion,omitempty" json:"schemaVersion,omitempty"`
 		Categories    []struct {
-			Name        string `yaml:"name"`
-			Description string `yaml:"description"`
+			Name        string `yaml:"name,omitempty" json:"name,omitempty"`
+			Description string `yaml:"description,omitempty" json:"description,omitempty"`
 			Actions     []struct {
-				Name               string   `yaml:"name"`
-				Body               bool     `yaml:"body"`
-				Method             string   `yaml:"method"`
-				Description        string   `yaml:"description"`
-				Parameters         []string `yaml:"parameters"`
-				OptionalParameters []string `yaml:"optionalParameters"`
-				Roles              []string `yaml:"roles"`
-			} `yaml:"actions"`
-		} `yaml:"categories"`
-	} `yaml:"requestSchema"`
+				Name               string   `yaml:"name,omitempty" json:"name,omitempty"`
+				Body               bool     `yaml:"body,omitempty" json:"body,omitempty"`
+				Method             string   `yaml:"method,omitempty" json:"method,omitempty"`
+				Description        string   `yaml:"description,omitempty" json:"description,omitempty"`
+				Parameters         []string `yaml:"parameters,omitempty" json:"parameters,omitempty"`
+				OptionalParameters []string `yaml:"optionalParameters,omitempty" json:"optionalParameters,omitempty"`
+				Headers            struct {
+					Request  []HeaderEntry `yaml:"request,omitempty" json:"request,omitempty"`
+					Response []HeaderEntry `yaml:"response,omitempty" json:"response,omitempty"`
+				} `yaml:"headers"`
+				Roles []string `yaml:"roles,omitempty" json:"roles,omitempty"`
+			} `yaml:"actions,omitempty" json:"actions,omitempty"`
+		} `yaml:"categories,omitempty" json:"categories,omitempty"`
+	} `yaml:"requestSchema,omitempty" json:"requestSchema,omitempty"`
+}
+
+type HeaderEntry struct {
+	Name        string `yaml:"name,omitempty" json:"name,omitempty"`
+	Description string `yaml:"description,omitempty" json:"description,omitempty"`
+	Required    bool   `yaml:"required,omitempty" json:"required,omitempty"`
 }
 
 // GetSchema unmarshals the request schema data and returns a Schema struct
