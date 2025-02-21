762698dc5f76aced73e65a653f81210b23cdd5f0
Mitchell Stanton <mitch@syntima.dev>
2025-02-21 12:30:19 -0500

    Refactor ESet function to remove unused return value from encryption; enhance clarity

--- redisTools/redisTools.go
+++ redisTools/redisTools.go
@@ -113,7 +113,7 @@ func ESet(key string, value string, expiration int, redisConfig RedisConfigurati
 	}
 	rCon := redis.NewClient(RedisOptions(redisConfig))
 	valueBytes := []byte(value)
-	_, encValue, err := encryption.Encrypt(valueBytes, redisEncryptionKey, false)
+	encValue, err := encryption.Encrypt(valueBytes, redisEncryptionKey, false)
 	if err != nil {
 		return err
 	}
